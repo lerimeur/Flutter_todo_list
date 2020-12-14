8a0be944d3b500de959df5e617a4de66e6c61270
Gregoire Brasseur <gregoirebrasseurpro@gmail.com>
2020-12-14 01:00:36 +0100

    feat: basic app

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import './Storage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
